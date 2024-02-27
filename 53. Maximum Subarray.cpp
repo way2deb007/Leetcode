077319f714e2337daa07bb748da0960cdde7e7f7
Debdulal Das <98931862+way2deb007@users.noreply.github.com>
2024-02-27 20:43:33 +0530

    Create 53. Maximum Subarray.cpp

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,14 @@
+int maxSubArray(vector<int>& nums) {
+        int max_so_far=INT_MIN, max_ending_here=0;
+        for(int i=0;i<nums.size();i++){
+            max_ending_here+=nums[i];
+            if(max_so_far<max_ending_here)
+                max_so_far=max_ending_here;
+            if(max_ending_here<0)
+                max_ending_here=0;
+        }
+        return max_so_far;       
+    }
+
+
+#Given an integer array nums, find the subarray with the largest sum, and return its sum.
